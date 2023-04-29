fbb95a08e9bd77f153483f431dd1bb253c16a2bd
Yashwanth <mvyashwanthkumar@gmail.com>
2023-04-29 13:07:34 +0530

    Final commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="https://th.bing.com/th/id/OIP.K-4RqDC6zFrpAG31ayDDOgHaHa?w=156&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7"className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+            <h4></h4>
+        </nav>
+    )
+}
